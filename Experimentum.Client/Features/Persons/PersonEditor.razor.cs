36090bc7db73aa01e90aee7a8085e1a0fa426966
David Eaton <DavidE@stocktrac.com>
2023-11-17 19:19:02 -0500

    Add validation code

--- Experimentum.Client/Features/Persons/PersonEditor.razor.cs
+++ Experimentum.Client/Features/Persons/PersonEditor.razor.cs
@@ -1,5 +1,8 @@
-﻿using Experimentum.Shared.Features.Persons;
+﻿using Experimentum.Shared.Features.Emails;
+using Experimentum.Shared.Features.Persons;
+using Experimentum.Shared.Features.Persons.PersonNames;
 using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
@@ -8,14 +11,28 @@ namespace Experimentum.Client.Features.Persons
     public partial class PersonEditor : ComponentBase
     {
         private readonly PersonRequest person = new();
-        private EditContext? _editContext;
-        [Inject] private IValidator<PersonRequest> _personValidator { get; set; }
+        private EditContext? editContext;
+        [Inject] private IValidator<PersonRequest>? PersonValidator { get; set; }
+        [Inject] private IValidator<EmailRequest>? EmailValidator { get; set; }
+        [Inject] private IValidator<PersonNameRequest>? PersonNameValidator { get; set; }
 
-        private string emailValidationMessage = string.Empty;
+        public PersonEditor() { }
+        public PersonEditor(IValidator<PersonRequest>? personValidator)
+        {
+            PersonValidator = personValidator;
+        }
+
+        private string? emailValidationMessage = string.Empty;
+        private string? lastNameValidationMessage = string.Empty;
+        private string? firstNameValidationMessage = string.Empty;
+        private string? middleNameValidationMessage = string.Empty;
+        private string? genderValidationMessage = string.Empty;
+        private string? birthdayValidationMessage = string.Empty;
+        private string? favoriteColorValidationMessage = string.Empty;
 
         protected override void OnInitialized()
         {
-            _editContext = new EditContext(person);
+            editContext = new EditContext(person);
         }
         private void SubmitForm()
         {
@@ -27,9 +44,65 @@ namespace Experimentum.Client.Features.Persons
             ValidateForm();
         }
 
+        private void ValidateField(string propertyName, Action<string> updateValidationMessage)
+        {
+            var validationResult = PersonValidator?.Validate(person);
+
+            if (validationResult is null || validationResult.IsValid)
+            {
+                updateValidationMessage(string.Empty);
+            }
+            else
+            {
+                var validationMessage = validationResult.Errors
+                    .FirstOrDefault(error => error.PropertyName == propertyName &&
+                                             !string.IsNullOrWhiteSpace(error.ErrorMessage))
+                    ?.ErrorMessage ?? string.Empty;
+
+                updateValidationMessage(validationMessage);
+            }
+        }
+
+        private void ValidateGender()
+        {
+            ValidateField(nameof(person.Gender), message => genderValidationMessage = message);
+        }
+
+        private void ValidateBirthday()
+        {
+            ValidateField(nameof(person.Birthday), message => birthdayValidationMessage = message);
+        }
+
+        private void ValidateFavoriteColor()
+        {
+            ValidateField(nameof(person.FavoriteColor), message => favoriteColorValidationMessage = message);
+        }
+
+        private void ValidateEmail()
+        {
+            var validationResult = EmailValidator?.Validate(person.Email);
+
+            if (validationResult is null || validationResult.IsValid)
+            {
+                emailValidationMessage = string.Empty;
+            }
+            else
+            {
+                emailValidationMessage = validationResult.Errors
+                    .FirstOrDefault(error => error.PropertyName == nameof(person.Email) &&
+                                             !string.IsNullOrWhiteSpace(error.ErrorMessage))
+                    ?.ErrorMessage ?? string.Empty;
+            }
+        }
+
         private void ValidateForm()
         {
-            var validationResult = _personValidator.Validate(person);
+            var validationResult = PersonValidator?.Validate(person);
+
+            if (validationResult is null)
+            {
+                return;
+            }
 
             if (!validationResult.IsValid)
             {
@@ -37,23 +110,63 @@ namespace Experimentum.Client.Features.Persons
                 {
                     if (!string.IsNullOrWhiteSpace(error.ErrorMessage) && !string.IsNullOrWhiteSpace(error.PropertyName))
                     {
-                        if (error.PropertyName == nameof(person.Email))
-                        {
-                            emailValidationMessage = error.ErrorMessage;
-                        }
+                        SetValidationMessages(error);
                     }
                 }
             }
             else
             {
-                emailValidationMessage = string.Empty;
+                ClearValidationMessages();
             }
         }
 
-        private string EmailInputCssClass =>
-            !string.IsNullOrWhiteSpace(emailValidationMessage)
-            ? "form-control is-invalid"
-            : "form-control";
+        private void SetValidationMessages(ValidationFailure? error)
+        {
+            if (error.PropertyName == nameof(person.Email))
+            {
+                emailValidationMessage = error.ErrorMessage;
+            }
 
+            if (error.PropertyName == nameof(person.Name))
+            {
+                lastNameValidationMessage = error.ErrorMessage;
+                firstNameValidationMessage = error.ErrorMessage;
+                middleNameValidationMessage = error.ErrorMessage;
+            }
+
+            if (error.PropertyName == nameof(person.Gender))
+            {
+                genderValidationMessage = error.ErrorMessage;
+            }
+
+            if (error.PropertyName == nameof(person.Birthday))
+            {
+                birthdayValidationMessage = error.ErrorMessage;
+            }
+
+            if (error.PropertyName == nameof(person.FavoriteColor))
+            {
+                favoriteColorValidationMessage = error.ErrorMessage;
+            }
+        }
+
+        private void ClearValidationMessages()
+        {
+            emailValidationMessage = string.Empty;
+            lastNameValidationMessage = string.Empty;
+            firstNameValidationMessage = string.Empty;
+            middleNameValidationMessage = string.Empty;
+            genderValidationMessage = string.Empty;
+            birthdayValidationMessage = string.Empty;
+            favoriteColorValidationMessage = string.Empty;
+        }
+
+        private string GenderInputCssClass => GetInputCssClass(genderValidationMessage);
+        private string BirthdayInputCssClass => GetInputCssClass(birthdayValidationMessage);
+        private string FavoriteColorInputCssClass => GetInputCssClass(favoriteColorValidationMessage);
+        private string EmailInputCssClass => GetInputCssClass(emailValidationMessage);
+
+        private string GetInputCssClass(string validationMessage) =>
+            !string.IsNullOrWhiteSpace(validationMessage) ? "form-control is-invalid" : "form-control";
     }
 }
