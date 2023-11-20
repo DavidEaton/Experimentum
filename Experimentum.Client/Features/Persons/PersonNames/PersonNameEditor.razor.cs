6cc3c3e2c5130536754418ef994684b9510230b4
David Eaton <DavidE@stocktrac.com>
2023-11-20 17:24:47 -0500

    Replace name inputs in PersonEditor with PersonNameEditor Component

--- Experimentum.Client/Features/Persons/PersonNames/PersonNameEditor.razor.cs
+++ Experimentum.Client/Features/Persons/PersonNames/PersonNameEditor.razor.cs
@@ -1,22 +1,10 @@
 ﻿using Experimentum.Shared.Features.Persons.PersonNames;
-using FluentValidation;
 using Microsoft.AspNetCore.Components;
 
 namespace Experimentum.Client.Features.Persons.PersonNames
 {
     public partial class PersonNameEditor : ComponentBase
     {
-        [Inject] private IValidator<PersonNameRequest>? PersonNameValidator { get; set; }
-        [Parameter] public PersonNameRequest personName { get; set; }
-
-        private void Validate()
-        {
-            var validationResult = PersonNameValidator?.Validate(personName);
-
-            if (validationResult is null)
-            {
-                return;
-            }
-        }
+        [Parameter] public PersonNameRequest Name { get; set; }
     }
 }
