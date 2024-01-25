e2a01b8a51d2989fd2b368c41665012df76affdc
David Eaton <DavidE@stocktrac.com>
2024-01-25 09:23:58 -0500

    Add edge case unit tests Add Azure Pipeline to run and publish unit tests and results

--- Experimentum.Tests/Unit/Features/Phones/PhoneContractExtensionsShould.cs
+++ Experimentum.Tests/Unit/Features/Phones/PhoneContractExtensionsShould.cs
@@ -89,11 +89,105 @@ namespace Experimentum.Tests.Unit.Features.Phones
                 PhoneType = PhoneType.Mobile
             };
 
-            var phone = phoneRequest.ToREntity();
+            var phone = phoneRequest.ToEntity();
 
             phone.Should().NotBeNull();
             phone!.Number.Should().Be(phoneRequest.Number);
             phone.PhoneType.Should().Be(phoneRequest.PhoneType);
+        }
+
+        [Fact]
+        public void Convert_null_phone_to_null_request()
+        {
+            Phone phone = null;
+
+            var request = phone.ToRequest();
+
+            request.Should().BeEquivalentTo(new PhoneRequest());
+        }
+
+        [Fact]
+        public void Convert_null_phone_response_to_null_request()
+        {
+            PhoneResponse phoneResponse = null;
+
+            var request = phoneResponse.ToRequest();
+
+            request.Should().BeEquivalentTo(new PhoneRequest());
+        }
+
+        [Fact]
+        public void Convert_null_phone_to_null_response()
+        {
+            Phone phone = null;
+
+            var response = phone.ToResponse();
+
+            response.Should().BeEquivalentTo(new PhoneResponse());
+        }
+
+        [Fact]
+        public void Convert_null_request_to_null_response()
+        {
+            PhoneRequest phoneRequest = null;
+
+            var response = phoneRequest.ToResponse();
+
+            response.Should().BeEquivalentTo(new PhoneResponse());
+        }
+
+        [Fact]
+        public void Convert_null_request_to_null_entity()
+        {
+            PhoneRequest phoneRequest = null;
+
+            var phone = phoneRequest.ToEntity();
+
+            phone.Should().BeNull();
+        }
+
+        [Fact]
+        public void Convert_invalid_request_to_null_entity()
+        {
+            var phoneRequest = new PhoneRequest
+            {
+                Number = "InvalidPhoneNumber",
+                PhoneType = PhoneType.Mobile
+            };
+
+            var phone = phoneRequest.ToEntity();
+
+            phone.Should().BeNull();
+        }
+
+        [Fact]
+        public void Return_null_entity_when_phone_number_is_empty()
+        {
+            var request = new PhoneRequest { Number = "", PhoneType = PhoneType.Mobile };
+
+            var result = request.ToEntity();
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Return_null_entity_when_phone_type_is_invalid()
+        {
+            var request = new PhoneRequest { Number = "123-456-7890", PhoneType = (PhoneType)(-1) };
+
+            var result = request.ToEntity();
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Return_null_entity_when_phone_number_and_type_are_invalid()
+        {
+            var request = new PhoneRequest { Number = "InvalidNumber", PhoneType = (PhoneType)(-1) };
+
+            var result = request.ToEntity();
+
+            result.Should().BeNull();
         }
     }
 }
