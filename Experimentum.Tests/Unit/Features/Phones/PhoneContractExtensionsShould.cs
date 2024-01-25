b6586ff42b783267814e6cc039a619406adfadd0
David Eaton <DavidE@stocktrac.com>
2024-01-25 17:28:41 -0500

    Fail_test_to_demonstrate_pipeline_test_Failure

--- Experimentum.Tests/Unit/Features/Phones/PhoneContractExtensionsShould.cs
+++ Experimentum.Tests/Unit/Features/Phones/PhoneContractExtensionsShould.cs
@@ -189,5 +189,11 @@ namespace Experimentum.Tests.Unit.Features.Phones
 
             result.Should().BeNull();
         }
+
+        [Fact]
+        public void Fail_test_to_demonstrate_pipeline_test_Failure()
+        {
+            true.Should().BeFalse();
+        }
     }
 }
